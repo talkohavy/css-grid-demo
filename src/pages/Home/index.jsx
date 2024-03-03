b1cfc07cdd2cff07c1b287e608ac7101252c3a7a
Tal Kohavy <talkohavy@gmail.com>
2024-03-03 13:42:22 +0200

    added a 5th example - grid-template-rows

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Example1 from './Example1';
 import Example2 from './Example2';
 import Example3 from './Example3';
 import Example4 from './Example4';
+import Example5 from './Example5';
 
 export default function HomePage() {
   return (
@@ -15,6 +16,8 @@ export default function HomePage() {
       <Example3 />
 
       <Example4 />
+
+      <Example5 />
     </div>
   );
 }
