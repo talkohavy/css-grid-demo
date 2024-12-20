cd59960f5b120cb892c8841f1bbd2cdae2d7ebb9
Tal Kohavy <talkohavy@gmail.com>
2024-12-20 09:08:13 +0200

    created Example2

--- src/pages/Example2/Example2.tsx
+++ src/pages/Example2/Example2.tsx
@@ -1,20 +1,22 @@
-export default function Example1() {
+export default function Example2() {
   return (
     <div className='flex size-full flex-col items-start justify-start gap-10 overflow-auto p-8 dark:text-white'>
-      <h1 className='text-3xl font-bold'>Example 1</h1>
+      <h1 className='text-3xl font-bold'>Example 2</h1>
 
       <div className='w-full space-y-4'>
         <p>
-          Using <code className='text-blue-600'>`grid-template-columns`</code> with 2 columns of hard-coded px width.
+          Using <code className='text-blue-600'>`gap`</code>
         </p>
 
-        <div className='rounded-lg border border-[#383838] p-4'>
+        <div className='rounded-lg border border-[#383838] bg-gray-50 p-4'>
           <pre>
             .gridContainer &#123;
             <br />
             &nbsp;&nbsp; display: grid;
             <br />
             &nbsp;&nbsp; grid-template-columns: 200px 100px;
+            <br />
+            &nbsp;&nbsp; gap: 10px;
             <br />
             &#125;
           </pre>
@@ -25,11 +27,13 @@ export default function Example1() {
           style={{
             display: 'grid',
             gridTemplateColumns: '200px 100px',
+            gap: 10,
           }}
         >
           <div className='outline outline-red-300'>I am the first column</div>
           <div className='outline outline-red-300'>I am the second column</div>
-          <div className='outline outline-red-300'>I am again on the first column</div>
+          <div className='outline outline-red-300'>Second row A</div>
+          <div className='outline outline-red-300'>Second row B</div>
         </div>
       </div>
     </div>
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
 
 const Example1 = lazy(() => import('./pages/Example1'));
+const Example2 = lazy(() => import('./pages/Example2'));
 
 export const routes = [
   {
@@ -8,5 +9,11 @@ export const routes = [
     text: 'Example 1',
     activeNames: ['/example-1', '/'],
     Component: Example1,
+  },
+  {
+    to: '/example-2',
+    text: 'Example 2',
+    activeNames: ['/example-2', '/'],
+    Component: Example2,
   },
 ];
