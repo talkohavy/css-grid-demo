35c6db3394b918b29a169ed375aac08d95d3d6e1
Tal Kohavy <talkohavy@gmail.com>
2024-12-20 09:49:20 +0200

    created Example5

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ const Example1 = lazy(() => import('./pages/Example1'));
 const Example2 = lazy(() => import('./pages/Example2'));
 const Example3 = lazy(() => import('./pages/Example3'));
 const Example4 = lazy(() => import('./pages/Example4'));
+const Example5 = lazy(() => import('./pages/Example5'));
 
 export const routes = [
   {
@@ -29,5 +30,11 @@ export const routes = [
     text: 'Example 4',
     activeNames: ['/example-4'],
     Component: Example4,
+  },
+  {
+    to: '/example-5',
+    text: 'Example 5',
+    activeNames: ['/example-5'],
+    Component: Example5,
   },
 ];
