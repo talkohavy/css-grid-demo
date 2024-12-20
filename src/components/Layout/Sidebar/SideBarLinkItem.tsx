391c699448423654d451e5451e4a6fa61cca676b
Tal Kohavy <talkohavy@gmail.com>
2024-12-20 09:56:16 +0200

    navLinks border

--- src/components/Layout/Sidebar/SideBarLinkItem.tsx
+++ src/components/Layout/Sidebar/SideBarLinkItem.tsx
@@ -11,7 +11,13 @@ export default function SideBarLinkItem(props: SideBarLinkItemProps) {
   const { to, text, isActive } = props;
 
   return (
-    <Link to={to} className={clsx('text-lg hover:text-red-500 active:text-red-400', isActive && 'font-bold')}>
+    <Link
+      to={to}
+      className={clsx(
+        'p-1 border rounded-md text-lg hover:text-red-500 active:text-red-400',
+        isActive ? 'text-blue-700 border-black dark:border-transparent dark:text-yellow-400' : 'border-transparent',
+      )}
+    >
       {text}
     </Link>
   );
