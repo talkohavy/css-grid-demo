a4bd730f4cd19e1c722868bbb2f691f0b90b4ff7
Tal Kohavy <talkohavy@gmail.com>
2024-12-25 14:55:30 +0200

    removed all unrelated files

--- README.md
+++
@@ -1,5 +0,0 @@
-# Grid Common Usage Examples
-
-This repo contains some of the most useful example you can achieve with grid layout.
-
-Use this project as a reference on how to use it wisely.
--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { lazy } from 'react';
-
-export const routes = Array.from(Array(12)).map((_, index) => ({
-  to: index === 0 ? '/' : `/example-${index + 1}`,
-  text: `Example ${index + 1}`,
-  activeNames: [`/example-${index + 1}`],
-  Component: lazy(() => import(`./pages/Example${index + 1}`)),
-}));
